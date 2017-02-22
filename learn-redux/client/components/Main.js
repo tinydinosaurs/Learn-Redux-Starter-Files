55f458187e24533ab4a4679b780e586908984cdd
Dana <dana.lachman@gmail.com>
2017-02-21 22:26:50 -0600

    set up core files and redux store

--- learn-redux/client/components/Main.js
+++ learn-redux/client/components/Main.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router';
 
-export default React.createClass({
-  render() {
-    return (
-      <div>
-        <h1><Link to='/'>cloudtype.me</Link></h1>
-      </div>
-    )
-  }
-})
+
+const Main = (props) => {
+  return (
+    <div>
+      <h1><Link to='/'>cloudtype.me</Link></h1>
+      {React.cloneElement(props.children, props)}
+    </div>
+  )
+}
+
+export default Main;
+
+
+// export default React.createClass({
+//   render() {
+//     return (
+//       <div>
+//         <h1><Link to='/'>cloudtype.me</Link></h1>
+//         {React.cloneElement(this.props.children, this.props)}
+//       </div>
+//     )
+//   }
+// })
---
+++ learn-redux/client/components/Single.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Single = (props) => {
+  return (
+    <div className="single-photo">
+      I'm a single photo
+    </div>
+  )
+}
+
+export default Single;
